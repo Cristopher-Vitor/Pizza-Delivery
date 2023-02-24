81da8b896854ed527cc49ffaa0313ea9a7c5bde5
Cristopher <cristophervga@hotmail.com>
2023-02-24 10:56:39 -0300

    adicionando footer e implementando modules no javascript

--- js/script.js
+++ js/script.js
@@ -1,25 +1,2 @@
-const btnMobile = document.querySelector('#btn-mobile');
-const headerMenu = document.querySelector('.header-menu');
-
-function toggleMenu(event){
-    if(event.type === 'touchstart')  event.preventDefault();
-    const nav = document.querySelector('#nav');
-    nav.classList.toggle('active');
-    const active = nav.classList.contains('active');
-    event.currentTarget.setAttribute('aria-expanded', active);
-    if(active){
-        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
-    } else{
-        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
-    }
-}
-
-btnMobile.addEventListener('click', toggleMenu);
-btnMobile.addEventListener('touchstart', toggleMenu);
-headerMenu.addEventListener('click', toggleMenu);
-
-
-
-
-
-
+import initMenuMobile from './modules/menu-mobile.js';
+initMenuMobile();
