73cea9bb77f39e5a272c177335f1a73018434faf
Cristopher <cristophervga@hotmail.com>
2023-02-27 09:41:04 -0300

    adicionando a funcionalidade do modal de login

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import initMenuMobile from './modules/menu-mobile.js';
 import ScrollSuave from './modules/scroll-suave.js';
-
+import Modal from './modules/modal.js';
 
 
 
@@ -8,3 +8,6 @@ initMenuMobile();
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
+
+const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]');
+modal.init();
